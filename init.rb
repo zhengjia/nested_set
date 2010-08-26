74f23da061e73a005c45f74e02b2b8a13b280e35
Igor Galeta <galeta.igor@gmail.com>
2010-08-26 18:07:19 +0800

    Fix initializer

--- init.rb
+++ init.rb
@@ -1,2 +1,2 @@
 # encoding: utf-8
-require 'awesome_nested_set'
+require File.join(File.dirname(__FILE__), "lib", "awesome_nested_set")
--- lib/awesome_nested_set/railtie.rb
+++ lib/awesome_nested_set/railtie.rb
@@ -6,19 +6,14 @@ module CollectiveIdea
   module Acts
     module NestedSet
       class Railtie < ::Rails::Railtie
-        initializer "awesome_nested_set.on_rails_init" do
+        config.before_initialize do
           ActiveSupport.on_load :active_record do
-            CollectiveIdea::Acts::NestedSet::Railtie.extend_active_record
+            ActiveRecord::Base.send(:include, CollectiveIdea::Acts::NestedSet::Base)
           end
 
           ActiveSupport.on_load :action_view do
             ActionView::Base.send(:include, CollectiveIdea::Acts::NestedSet::Helper)
           end
-        end
-
-        def self.extend_active_record
-          ActiveRecord::Base.send :include,
-            CollectiveIdea::Acts::NestedSet::Base
         end
       end
     end
